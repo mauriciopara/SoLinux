7bbaf943b4f42fe84a99a58ebd816f7492dd9848
mauriciopara <paramauricio7@gmail.com>
2024-11-19 22:37:59 -0500

    final

--- trabalho.sh
+++ trabalho.sh
@@ -13,3 +13,13 @@ cp /etc/passwd /etc/group maria/
 cp -r joao joao_copia
 
 cp -r maria maria_copia
+
+mkdir mais_uma_pasta
+
+cp -r joao maria mais_uma_pasta/
+
+mv mais_uma_pasta/maria  mais_uma_pasta/temp
+
+mv mais_uma_pasta/joao mais_uma_pasta/maria
+
+mv mais_uma_pasta/temp mais_uma_pasta/joao

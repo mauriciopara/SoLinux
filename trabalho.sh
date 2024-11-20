9879b980169ebf18dce4f216b2f1060dd7e2f5b6
mauriciopara <paramauricio7@gmail.com>
2024-11-19 21:25:28 -0500

    teste

--- trabalho.sh
+++ trabalho.sh
@@ -2,8 +2,14 @@
 
 # NAO APAGUE A LINHA ACIMA!
 
-mkdir -p joao
-mkdir  mauricio
-ls
-dd
-linux 
+cd ~
+
+mkdir joao maria
+
+cp -r /etc/systemd joao/
+
+cp /etc/passwd /etc/group maria/
+
+cp -r joao joao_copia
+
+cp -r maria maria_copia
